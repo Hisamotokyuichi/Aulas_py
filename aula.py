e21212f49f3c36fc22de389db5a70c2e7d74a24e
Hisamotokyuichi <hisamoto91@gmail.com>
2024-04-13 23:41:50 -0300

    fist commit

---
+++ aula.py
@@ -0,0 +1,16 @@
+nome = "isaque"
+idade = 25 
+
+
+nome = "isaque hisamoto"
+
+
+print(nome,idade)
+
+
+
+BRAZILIAN_STATES  = ["SP","SC","RJ","RS"]
+BRAZILIAN_STATES = 10
+
+
+print(BRAZILIAN_STATES)
